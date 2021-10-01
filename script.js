bd03c0664ba068c0c702b6908aed6643011822b3
Tolya Fam <famovkin@yandex.ru>
2021-10-02 01:29:07 +0300

    feat: добавить возможность добавления новых todo

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const todos = [
   "Решить задачу на Codewars",
 ];
 
-const todoListElement = document.querySelector('.todos__list');
+const todoListElement = document.querySelector(".todos__list");
+const todoFormElement = document.querySelector(".todos__form");
+
+todoFormElement.addEventListener("submit", addTodo);
 
 function renderTodo(todoText) {
   const todo = `<li class="todo todos__todo">
@@ -19,7 +22,17 @@ function renderTodo(todoText) {
     <button class="todo__btn todo__btn_type_delete"></button>
   </div>
   </li>`;
-  todoListElement.insertAdjacentHTML('afterbegin', todo)
+  todoListElement.insertAdjacentHTML("afterbegin", todo);
+}
+
+function addTodo(event) {
+  event.preventDefault();
+
+  const newTodoText = event.currentTarget.querySelector(".todos__input").value;
+
+  renderTodo(newTodoText);
+
+  event.currentTarget.reset();
 }
 
 todos.map(renderTodo);
