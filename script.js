7bf1b4923ccc6cf946020b27a8287a2f3afbd9d4
Tolya Fam <famovkin@yandex.ru>
2021-10-02 01:50:20 +0300

    refactor: использовать шаблоны для добавления todo

--- script.js
+++ script.js
@@ -10,19 +10,16 @@ const todos = [
 
 const todoListElement = document.querySelector(".todos__list");
 const todoFormElement = document.querySelector(".todos__form");
+const todoTemplateElement = document.querySelector(".todo-template");
 
 todoFormElement.addEventListener("submit", addTodo);
 
 function renderTodo(todoText) {
-  const todo = `<li class="todo todos__todo">
-  <p class="todo__text">${todoText}</p>
-  <div class="todo__buttons">
-    <button class="todo__btn todo__btn_type_edit"></button>
-    <button class="todo__btn todo__btn_type_dublicate"></button>
-    <button class="todo__btn todo__btn_type_delete"></button>
-  </div>
-  </li>`;
-  todoListElement.insertAdjacentHTML("afterbegin", todo);
+  const newTodo = todoTemplateElement.content.cloneNode(true);
+
+  newTodo.querySelector(".todo__text").textContent = todoText;
+
+  todoListElement.prepend(newTodo);
 }
 
 function addTodo(event) {
